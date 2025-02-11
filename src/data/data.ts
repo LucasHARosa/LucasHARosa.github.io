9f1acad1f520b7c3d56e6561ac24995dcd0a66ad
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2025-02-11 17:32:27 -0300

    feat: update data

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,12 +3,33 @@ export const listaProjetos = [
     id: 15,
     titulo: "Quiz Programing",
     descricao: "Aplicativo mobile de quiz sobre programação, oferecendo perguntas dinâmicas e desafios para testar conhecimentos em diversas linguagens e conceitos.",
-    tags: ["React Native", "TypeScript", "Native Base"],
+    tags: ["React Native", "Native Base"],
     imagem: "/imagens/quizprogram.png",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/Quiz_Programing",
+    LinkGit: "https://github.com/LucasHARosa/Quiz-Programing",
     tipo: ["mobile"]
   },
+  {
+    id: 18,
+    titulo: "I Weather",
+    descricao: "Uma aplicação React Native que fornece informações meteorológicas em tempo real. Aplicação de testes unitários e automatizados ",
+    tags: ["React Native", "Jest"],
+    imagem: "/imagens/iweather.png",
+    Link: "",
+    LinkGit: "https://github.com/LucasHARosa/IWeather",
+    tipo: ["mobile"]
+  },
+  {
+    id: 19,
+    titulo: "Keyboard Test",
+    descricao: "Teste de teclado qual tecla está sendo pressionada em um teclado 65% com som e histórico de teclas. Idealmente feito para testar meus teclados mecânicos.",
+    tags: ["React", "NextJs"],
+    imagem: "/imagens/testKeyboard.png",
+    Link: "https://keyboard-test-eight.vercel.app/",
+    LinkGit: "",
+    tipo: ["frontend"]
+  },
+
   {
     id: 14,
     titulo: "Gym Train",
@@ -16,7 +37,7 @@ export const listaProjetos = [
     tags: ["React Native", "TypeScript", "Styled Components"],
     imagem: "/imagens/gyntrain.png",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/gym_train",
+    LinkGit: "https://github.com/LucasHARosa/Gym-Train",
     tipo: ["mobile"]
   },
   {
@@ -26,7 +47,7 @@ export const listaProjetos = [
     tags: ["React", "TypeScript", "json-server"],
     imagem: "/imagens/dt_money.png",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/DT_Money",
+    LinkGit: "https://github.com/LucasHARosa/DT-Money",
     tipo: ["frontend"]
   },
   {
@@ -35,8 +56,8 @@ export const listaProjetos = [
     descricao: "Timer para organização de tarefas com foco na técnica Pomodoro.",
     tags: ["React", "TypeScript"],
     imagem: "/imagens/timer_pomoro_1.png",
-    Link: "https://lucasharosa.github.io/Timer_Pomodoro/",
-    LinkGit: "https://github.com/LucasHARosa/Timer_Pomodoro",
+    Link: "https://lucasharosa.github.io/Timer-Pomodoro/",
+    LinkGit: "https://github.com/LucasHARosa/Timer-Pomodoro",
     tipo: ["frontend"]
   },
   {
@@ -55,8 +76,8 @@ export const listaProjetos = [
     descricao: "Aplicação de e-commerce para compra de cafés, simulando um sistema de pedidos com carrinho de compras e cálculo de preços.",
     tags: ["React", "TypeScript"],
     imagem: "/imagens/coffe_deliveryHome.png",
-    Link: "https://lucasharosa.github.io/Coffee_delivery/",
-    LinkGit: "https://github.com/LucasHARosa/Coffe_delivery",
+    Link: "https://lucasharosa.github.io/Coffee-Delivery/",
+    LinkGit: "https://github.com/LucasHARosa/Coffe-Delivery",
     tipo: ["frontend"]
   },
   {
@@ -76,7 +97,7 @@ export const listaProjetos = [
     tags: ["CNN", "TCC", "YOLOV8"],
     imagem: "/imagens/tcc.PNG",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/IA_GMAW_TG_UnB",
+    LinkGit: "https://github.com/LucasHARosa/IA-GMAW-TG-UnB",
     tipo: ["ia", "principais"]
   },
   {
@@ -86,7 +107,7 @@ export const listaProjetos = [
     tags: ["Python", "Redes Neurais"],
     imagem: "https://static.imasters.com.br/wp-content/uploads/2016/06/redes-1.png",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/IA_Classification",
+    LinkGit: "https://github.com/LucasHARosa/IA-Classification",
     tipo: ["ia"]
   },
   {
@@ -96,7 +117,7 @@ export const listaProjetos = [
     tags: ["Python", "Redes Neurais"],
     imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXAcZTGYRe0OSumsIhXkiXB44sVkhC-X64ig&usqp=CAU",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/IA_Regression",
+    LinkGit: "https://github.com/LucasHARosa/IA-Regression",
     tipo: ["ia"]
   },
   {
@@ -106,7 +127,7 @@ export const listaProjetos = [
     tags: ["Python", "Redes Neurais", "CNN"],
     imagem: "https://www.sakurai.dev.br/assets/images/posts/2017-12-20-cnn-mapreduce.png",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/IA_convolution",
+    LinkGit: "https://github.com/LucasHARosa/IA-Convolution",
     tipo: ["ia"]
   },
   {
@@ -116,7 +137,7 @@ export const listaProjetos = [
     tags: ["CNN", "Redes Neurais", "Ruído"],
     imagem: "/imagens/ruidos.PNG",
     Link: "",
-    LinkGit: "https://github.com/LucasHARosa/IA_ruidos",
+    LinkGit: "https://github.com/LucasHARosa/IA-Ruidos",
     tipo: ["ia"]
   },
   {
@@ -125,8 +146,8 @@ export const listaProjetos = [
     descricao: "Simula um feed de rede social com postagens, comentários e curtidas.",
     tags: ["React", "TypeScript"],
     imagem: "/imagens/telaInicial.png",
-    Link: "https://lucasharosa.github.io/Ignite_Feed/",
-    LinkGit: "https://github.com/LucasHARosa/Ignite_Feed",
+    Link: "https://lucasharosa.github.io/Ignite-Feed/",
+    LinkGit: "https://github.com/LucasHARosa/Ignite-Feed",
     tipo: ["frontend"]
   },
   {
@@ -135,8 +156,8 @@ export const listaProjetos = [
     descricao: "E-commerce de jogos com filtros e informações detalhadas.",
     tags: ["React", "JavaScript"],
     imagem: "/imagens/Game_tracker_web.png",
-    Link: "https://lucasharosa.github.io/Game_tracker/",
-    LinkGit: "https://github.com/LucasHARosa/Game_tracker",
+    Link: "https://lucasharosa.github.io/Game-Tracker/",
+    LinkGit: "https://github.com/LucasHARosa/Game-Tracker",
     tipo: ["frontend"]
   },
   {
@@ -145,14 +166,14 @@ export const listaProjetos = [
     descricao: "Loja virtual de produtos de tecnologia com integração de API, onde os usuários podem visualizar produtos, adicionar ao carrinho e simular pedidos.",
     tags: ["React", "TypeScript", "Frame Motion"],
     imagem: "/imagens/home.png",
-    Link: "https://lucasharosa.github.io/MKS_SISTEMAS/",
-    LinkGit: "https://github.com/LucasHARosa/MKS_SISTEMAS",
+    Link: "",
+    LinkGit: "https://github.com/LucasHARosa/MKS-Sistemas",
     tipo: ["frontend", "principais"]
   },
   {
     id: 10,
     titulo: "Teams",
-    descricao: "Aplicativo mobile para gerenciamento de times e eventos.",
+    descricao: "Aplicativo mobile para gerenciamento de times e eventos, é possível criar turmas de jogos e atividades e dividir pessoas entre as duas turmas",
     tags: ["React Native", "TypeScript"],
     imagem: "/imagens/teams.PNG",
     Link: "",
@@ -162,11 +183,11 @@ export const listaProjetos = [
   {
     id: 6,
     titulo: "To do List",
-    descricao: "Lista de tarefas para cadastrar, concluir e excluir atividades.",
+    descricao: "Lista de tarefas para facilitar e organizar as atividades diárias, com opções de adicionar, remover e marcar como concluída.",
     tags: ["React", "React Native"],
     imagem: "/imagens/todolist.png",
-    Link: "https://lucasharosa.github.io/To_Do_List/",
-    LinkGit: "https://github.com/LucasHARosa/To_Do_List",
+    Link: "https://lucasharosa.github.io/To-Do-List/",
+    LinkGit: "https://github.com/LucasHARosa/To-Do-List",
     tipo: ["frontend", "mobile"]
   },
 ];
