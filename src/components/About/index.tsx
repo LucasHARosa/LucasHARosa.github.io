11a5d1e802acdbae7dc878791ff1498a9da81ca6
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2025-02-11 18:39:53 -0300

    feat: ajustar cálculos de movimento e melhorar a responsividade do componente About

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,45 +2,45 @@ import foto from '../../assets/foto.png'
 import { Heart, ChatCircle, PaperPlaneTilt } from 'phosphor-react';
 import { Tag } from '../Tag';
 import { ContainerAbout, ContainerAboutImage, ContainerAboutText, LegendImage } from './styles';
-import {  useRef} from 'react';
+import { useRef } from 'react';
 
 import { useGesture } from "react-use-gesture";
-import { useSpring, animated, to} from "@react-spring/web";
+import { useSpring, animated, to } from "@react-spring/web";
 
 
-export function About(){
+export function About() {
   const domTarget = useRef<HTMLDivElement>(null);
- 
+
 
   const calcX = (y: number) => {
     if (!domTarget.current) return;
     const cardRect = domTarget.current.getBoundingClientRect();
-    const centery = (cardRect.top + cardRect.bottom) /2;
-    return -(y - centery) / 55;
+    const centery = (cardRect.top + cardRect.bottom) / 2;
+    return -(y - centery) / 85;
   }
-  
+
   const calcY = (x: number) => {
     if (!domTarget.current) return;
     const cardRect = domTarget.current.getBoundingClientRect();
-    const centerx = (cardRect.left + cardRect.right) /2;
-    return (x - centerx) / 55;
+    const centerx = (cardRect.left + cardRect.right) / 2;
+    return (x - centerx) / 85;
   }
 
-  
-  
-  const [{ x, y, rotateX, rotateY,rotateZ,scale,zoom}, api] = useSpring(
+
+
+  const [{ x, y, rotateX, rotateY, rotateZ, scale, zoom }, api] = useSpring(
     () => ({
       rotateX: 0,
       rotateY: 0,
       rotateZ: 0,
       x: 0,
       y: 0,
-      scale:1,
+      scale: 1,
       zoom: 0,
       config: { mass: 10, tension: 500, friction: 40 },
     }),
   );
-  
+
   useGesture(
     {
       onMove: ({ xy: [px, py], dragging }) =>
@@ -50,10 +50,10 @@ export function About(){
           rotateY: calcY(px),
           scale: 1.05,
         }),
-        onHover: ({ hovering }) =>
+      onHover: ({ hovering }) =>
         !hovering && api({ rotateX: 0, rotateY: 0, scale: 1 }),
     },
-    { domTarget,eventOptions: { passive: false }},
+    { domTarget, eventOptions: { passive: false } },
   );
 
   return (
@@ -75,8 +75,8 @@ export function About(){
             <img src={foto} alt="" />
             <LegendImage>
               <Heart size={24} weight="fill" color="#e52e4d" />
-              <ChatCircle size={24}  color="#FFF" />
-              <PaperPlaneTilt size={24}  color="#FFF" />
+              <ChatCircle size={24} color="#FFF" />
+              <PaperPlaneTilt size={24} color="#FFF" />
             </LegendImage>
           </ContainerAboutImage>
         </animated.div>
@@ -90,7 +90,7 @@ export function About(){
           </div>
         </ContainerAboutText>
       </ContainerAbout>
-      
+
     </section>
   )
 }
