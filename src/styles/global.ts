dc55e3104c9565fa9e6da31a6bdc62a089dfd276
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2025-04-01 15:39:16 -0300

    feat: ajustar line-height global para melhorar a legibilidade

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,7 +30,7 @@ export const GlobalStyle = createGlobalStyle`
   body, input, textarea, button {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
-    line-height: 1;
+    line-height: 1.4;
   }
 
   h1, h2, h3, h4, h5, h6 {
